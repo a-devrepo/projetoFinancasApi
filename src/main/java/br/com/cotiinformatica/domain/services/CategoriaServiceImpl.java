6c477c8a4ccb91476b0b6b5c36b0256e9e0211f3
alison <adevpr11@gmail.com>
2025-07-23 10:51:29 -0300

    feat: implementa método criarCategoria

---
+++ src/main/java/br/com/cotiinformatica/domain/services/CategoriaServiceImpl.java
@@ -0,0 +1,39 @@
+package br.com.cotiinformatica.domain.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.domain.dtos.CategoriaRequest;
+import br.com.cotiinformatica.domain.dtos.CategoriaResponse;
+import br.com.cotiinformatica.domain.entities.Categoria;
+import br.com.cotiinformatica.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+	private CategoriaRepository categoriaRepository;
+
+	public CategoriaServiceImpl(CategoriaRepository categoriaRepository) {
+		this.categoriaRepository = categoriaRepository;
+	}
+
+	@Override
+	public CategoriaResponse criarCategoria(CategoriaRequest request, UUID usuarioId) {
+
+		var categoria = new Categoria();
+		categoria.setId(UUID.randomUUID());
+		categoria.setNome(request.nome());
+		categoria.setUsuarioId(usuarioId);
+		categoriaRepository.save(categoria);
+		return new CategoriaResponse(categoria.getId(), categoria.getNome());
+	}
+
+	@Override
+	public List<CategoriaResponse> consultarCategorias(UUID usuarioId) {
+
+		return null;
+	}
+
+}
