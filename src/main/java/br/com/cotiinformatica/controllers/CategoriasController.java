b25a0b50630585796922917e31c6faf2c4a1d5c8
alison <adevpr11@gmail.com>
2025-07-31 19:21:51 -0300

    chore: configura o Swagger para suportar autenticação JWT

--- src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
@@ -14,10 +14,17 @@ import br.com.cotiinformatica.components.JwtComponent;
 import br.com.cotiinformatica.domain.dtos.CategoriaRequest;
 import br.com.cotiinformatica.domain.dtos.CategoriaResponse;
 import br.com.cotiinformatica.domain.services.CategoriaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/v1/categorias")
+@Tag(name = "Controle de categorias", description = "Serviços para gerenciamento de categorias.")
+@SecurityRequirement(name = "bearerAuth") 
 public class CategoriasController {
 
 	private CategoriaService categoriaService;
@@ -27,13 +34,27 @@ public class CategoriasController {
 	}
 
 	@PostMapping
-	public ResponseEntity<CategoriaResponse> post(@RequestBody CategoriaRequest categoriaRequest, HttpServletRequest httpRequest) {
+	@Operation(
+	    summary = "Cadastro de categorias",
+	    description = "Cadastra uma nova categoria no sistema"
+	)
+	@io.swagger.v3.oas.annotations.parameters.RequestBody(
+	    description = "Dados da categoria a ser cadastrada",
+	    required = true,
+	    content = @Content(
+	        mediaType = "application/json",
+	        schema = @Schema(implementation = CategoriaRequest.class)
+	    )
+	)
+	public ResponseEntity<CategoriaResponse> post(@RequestBody CategoriaRequest categoriaRequest,
+			HttpServletRequest httpRequest) {
 		var usuarioId = (UUID) httpRequest.getAttribute("userId");
 		var response = categoriaService.criarCategoria(categoriaRequest, usuarioId);
 		return ResponseEntity.ok(response);
 	}
 
 	@GetMapping
+	@Operation(summary = "Listar categorias", description = "Retorna todas as categorias cadastradas pelo usuário autenticado")
 	public ResponseEntity<List<CategoriaResponse>> getAll(HttpServletRequest httpRequest) {
 		var usuarioId = (UUID) httpRequest.getAttribute("userId");
 		var response = categoriaService.consultarCategorias(usuarioId);
