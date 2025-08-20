ad20e5abbaca2adbba8cac6f0f5e2521b5acb9b7
alison <adevpr11@gmail.com>
2025-08-20 17:26:17 -0300

    docs: adiciona anotação em MovimentacaoResponse para formatar o campo data

--- src/main/java/br/com/cotiinformatica/domain/dtos/MovimentacaoResponse.java
+++ src/main/java/br/com/cotiinformatica/domain/dtos/MovimentacaoResponse.java
@@ -3,9 +3,15 @@ package br.com.cotiinformatica.domain.dtos;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record MovimentacaoResponse(
 		UUID id,
 		String nome,
+		@Schema(type = "string", example = "20/08/2025", description = "dd/MM/yyyy")
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
 		LocalDate data,
 		Double valor,
 		String tipo,
